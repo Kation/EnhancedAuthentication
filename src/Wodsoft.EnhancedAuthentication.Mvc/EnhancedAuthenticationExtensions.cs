48c49c9aa523f038e9766cc7e020c95b398bc9e1
Kation <kation@outlook.com>
2017-02-19 15:01:40 +0800

    改善体验

---
+++ src/Wodsoft.EnhancedAuthentication.Mvc/EnhancedAuthenticationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wodsoft.EnhancedAuthentication;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class EnhancedAuthenticationExtensions
+    {
+        public static void AddEnhancedAuthenticationService<IUserProvider>(this IServiceCollection services, EnhancedAuthenticationCertificate rootCert, IEnhancedAuthenticationCertificateProvider certProvider)
+            where IUserProvider : class, IEnhancedAuthenticationUserProvider
+        {
+            services.AddSingleton<EnhancedAuthenticationCertificate>(rootCert);
+            services.AddSingleton<IEnhancedAuthenticationCertificateProvider>(certProvider);
+            services.AddSingleton<EnhancedAuthenticationService>();
+            services.AddScoped<IEnhancedAuthenticationUserProvider, IUserProvider>();
+        }
+    }
+}
